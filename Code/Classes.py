fcd197925d19c8cb8ca65197a59d236e292a6b36
William Chermanne <william.chermanne@hotmail.com>
2019-06-12 23:56:04 +0200

    Compatibility for Mac

--- Code/Classes.py
+++ Code/Classes.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-`
+
 class Product:
 	def __init__(self, name):
 		self.name = name
@@ -5,45 +7,46 @@ class Product:
 		self.PriceOfIngredients = [] # Prix par kg
 		self.QuantityOfIngredients = []	# Quantité en kg
 		self.Value = 0
-	
+
 	def add_ingredient(self,ingredient, price, quantity):
 		self.ListOfIngredients.append(ingredient)
 		self.PriceOfIngredients.append(price)
 		self.QuantityOfIngredients.append(quantity)
-	
+
 	def show_ingredients(self):
 		print("Product:",self.name)
 		print("\n")
 		for item in range(len(self.ListOfIngredients)):
 			print(self.ListOfIngredients[item],"\n","Prix:",self.PriceOfIngredients[item], "€/kg", "Quantity:",self.QuantityOfIngredients[item],"kg")
 
-			
+
 	def compute_value(self,ActiveTime,coefficient):
 		IngredientsValue=0
 		for item in range(len(self.ListOfIngredients)):
 			IngredientsValue += (self.PriceOfIngredients[item])*(self.QuantityOfIngredients[item])
 		self.Value= IngredientsValue + coefficient*ActiveTime
-		
+
 	def display_infos(self):
 		print(self.name)
-		print(self.Value, "Moneco")	
-		
-		
+		print(self.Value, "Moneco")
+
+
 class Ingredient:
 	def __init__(self, name, price, quantity):
 		self.name = name
 		self.price = price
 		self.quantity = quantity
-		
+
 class Receipt:
 	RECEIPT_COUNT = 0
 	def __init__(self):
 		Receipt.RECEIPT_COUNT+=1
 		self.ID=Receipt.RECEIPT_COUNT
+		self.ingredients_list=[]
 
 class Request:
 	REQUEST_COUNT = 0
-		
+
 	def __init__(self, title, creator, product, quantity):
 		self.title= title
 		self.creator=creator
@@ -52,12 +55,12 @@ class Request:
 		self.status = "pending"
 		Request.REQUEST_COUNT+= 1
 		self.ID=Request.REQUEST_COUNT
-		
+
 	def display_infos(self):
 		print("Request number",self.ID,"from",self.creator)
 		print("Product",self.product)
 		print("Quantity",self.quantity)
-	
+
 class Person:
 	def __init__(self, name, pseudo, age, specialty):
 		self.name = name
@@ -65,7 +68,7 @@ class Person:
 		self.age = age
 		self.specialty = specialty
 		self.wallet = 0
-	
+
 	def display_infos(self):
 		print(" ")
 		print("Name:",self.name)
@@ -74,13 +77,13 @@ class Person:
 		print("Specialty:",self.specialty)
 		print("Wallet:",self.wallet)
 		print(" ")
-		
+
 	def give_money(self,amount):
 		self.wallet -=amount
-		
-	def receive_money(self, amount):	
+
+	def receive_money(self, amount):
 		self.wallet +=amount
-	
+
 	def create_request(self,title,product,quantity):
 		request = Request(self.name,title,product,quantity)
 		return request
@@ -88,22 +91,17 @@ class Person:
 
 class RequestManager:
 	ACTIVE_REQUEST = 0
-		
+
 	def __init__(self):
 		self.request_list=[]
-		
+
 	def add_request(self,request):
 		self.request_list.append(request)
-			
+
 	def remove_request(self,request):
 		self.request_list.remove(request)
-		
+
 	def display_request_list(self):
 		for i in range(len(self.request_list)):
 			request = (self.request_list[i])
 			request.display_infos()
-
-				
-		
-			
-	
