5b465260d398dede09a7276339bb7e6f184338d8
william.chermanne <william.chermanne@hotmail.com>
2019-06-13 13:45:46 +0200

    Added recipe and ingredient classes

--- Code/Classes.py
+++ Code/Classes.py
@@ -1,46 +1,69 @@
+		
+class Ingredient:
+	def __init__(self, name, quantity, price):
+		self.name = name
+		self.price = price
+		self.quantity = quantity
+		
+	def display_infos(self):
+		print("Name:",self.name)
+		print("Quantity:",self.quantity)
+		print("Price:",self.price)
+		print(" ")
+		
+
+class Recipe:
+	RECIPE_COUNT = 0
+	def __init__(self, name, creator):
+		Recipe.RECIPE_COUNT+=1
+		self.ID=Recipe.RECIPE_COUNT
+		self.ingredient_list=[]
+		self.name = name
+		self.creator = creator
+		self.comment = ""
+	
+	def add_comment(self,comment):
+		self.comment = comment
+		
+	def add_ingredient(self,ingredient):
+		self.ingredient_list.append(ingredient)
+	
+	def remove_ingredient(self, ingredient):
+		self.ingredient_list.remove(ingredient)
+		
+	def display_infos(self):
+		print("")
+		print("Recipe from",self.creator.name, ":")
+		for i in range(len(self.ingredient_list)):
+			ingredient = (self.ingredient_list[i])
+			print("Ingredient number",i+1)
+			ingredient.display_infos()
+		print(self.comment)
+
 class Product:
 	def __init__(self, name):
 		self.name = name
-		self.ListOfIngredients = []
-		self.PriceOfIngredients = [] # Prix par kg
-		self.QuantityOfIngredients = []	# Quantité en kg
-		self.Value = 0
+		self.recipe_list = []
+		self.selected_recipe = None 
+		self.value = 0
 	
-	def add_ingredient(self,ingredient, price, quantity):
-		self.ListOfIngredients.append(ingredient)
-		self.PriceOfIngredients.append(price)
-		self.QuantityOfIngredients.append(quantity)
-	
-	def show_ingredients(self):
+	def add_recipe(self,recipe):
+		self.recipe_list.append(recipe)
+		
+	def remove_recipe(self,recipe):
+		self.recipe_list.remove(recipe)
+		
+	def show_recipes(self):
 		print("Product:",self.name)
-		print("\n")
-		for item in range(len(self.ListOfIngredients)):
-			print(self.ListOfIngredients[item],"\n","Prix:",self.PriceOfIngredients[item], "€/kg", "Quantity:",self.QuantityOfIngredients[item],"kg")
-
-			
-	def compute_value(self,ActiveTime,coefficient):
-		IngredientsValue=0
-		for item in range(len(self.ListOfIngredients)):
-			IngredientsValue += (self.PriceOfIngredients[item])*(self.QuantityOfIngredients[item])
-		self.Value= IngredientsValue + coefficient*ActiveTime
+		for i in range(len(self.recipe_list)):
+			recipe = (self.recipe_list[i])
+			print("Recipe number",recipe.ID)
+			recipe.display_infos()
 		
 	def display_infos(self):
 		print(self.name)
 		print(self.Value, "Moneco")	
 		
-		
-class Ingredient:
-	def __init__(self, name, price, quantity):
-		self.name = name
-		self.price = price
-		self.quantity = quantity
-		
-class Receipt:
-	RECEIPT_COUNT = 0
-	def __init__(self):
-		Receipt.RECEIPT_COUNT+=1
-		self.ID=Receipt.RECEIPT_COUNT
-
 class Request:
 	REQUEST_COUNT = 0
 		
@@ -58,34 +81,6 @@ class Request:
 		print("Product",self.product)
 		print("Quantity",self.quantity)
 	
-class Person:
-	def __init__(self, name, pseudo, age, specialty):
-		self.name = name
-		self.pseudo = pseudo
-		self.age = age
-		self.specialty = specialty
-		self.wallet = 0
-	
-	def display_infos(self):
-		print(" ")
-		print("Name:",self.name)
-		print("Pseudo:",self.pseudo)
-		print("Age:",self.age)
-		print("Specialty:",self.specialty)
-		print("Wallet:",self.wallet)
-		print(" ")
-		
-	def give_money(self,amount):
-		self.wallet -=amount
-		
-	def receive_money(self, amount):	
-		self.wallet +=amount
-	
-	def create_request(self,title,product,quantity):
-		request = Request(self.name,title,product,quantity)
-		return request
-
-
 class RequestManager:
 	ACTIVE_REQUEST = 0
 		
@@ -103,7 +98,33 @@ class RequestManager:
 			request = (self.request_list[i])
 			request.display_infos()
 
-				
+class Person:
+	def __init__(self, name, pseudo, age, specialty):
+		self.name = name
+		self.pseudo = pseudo
+		self.age = age
+		self.specialty = specialty
+		self.wallet = 0
+		
+	def give_money(self,amount):
+		self.wallet -=amount
+		
+	def receive_money(self, amount):	
+		self.wallet +=amount
+	
+	def create_request(self,title,product,quantity):
+		request = Request(self.name,title,product,quantity)
+		return request	
+
+	def display_infos(self):
+		print(" ")
+		print("Name:",self.name)
+		print("Pseudo:",self.pseudo)
+		print("Age:",self.age)
+		print("Specialty:",self.specialty)
+		print("Wallet:",self.wallet)
+		print(" ")
+						
 		
 			
 	
--- Code/Main.py
+++ Code/Main.py
@@ -4,10 +4,19 @@ RM = Classes.RequestManager()
 William = Classes.Person("William","Will",23, "Pasta")
 Lola=Classes.Person("Lola","BabyLola",21, "Hummus")
 
+Hummus = Classes.Ingredient("Hummus",1,0.1)
+Eau = Classes.Ingredient("Eau",0.5,0)
+
+
+RecetteHummus = Classes.Recipe("La recette de Hummus de Lola",Lola)
+RecetteHummus.add_ingredient(Hummus)
+RecetteHummus.add_ingredient(Eau)
+RecetteHummus.display_infos()
+
 Hummus = Classes.Product("Hummus")
-Hummus.add_ingredient("PoisChiches",4,0.5)
-Hummus.add_ingredient("Eau",1,1)
-Hummus.compute_value(1,10)
+Hummus.add_recipe(RecetteHummus)
+Hummus.show_recipes()
+
 
 MyRequest= William.create_request("J'aimerais du Hummus","Hummus",0.5)
 RM.add_request(MyRequest)
